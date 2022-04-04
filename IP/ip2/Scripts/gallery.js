cc9861d3b910375e96f4e0c571c7b539c9b94c0c
Timur Gayazov <tim.gayazov@gmail.com>
2022-04-04 22:11:28 +0400

    corrected comments on the project

--- IP/ip2/Scripts/gallery.js
+++ IP/ip2/Scripts/gallery.js
@@ -1,35 +1,8 @@
-let img = ['image_1', 'image_2', 'image_3'];
+let img = ['image_1', 'image_2', 'image_3']; // массив в котором хранятся названия классов
 
-// let k = 0;
+let k = 0; // счетчик для определения позиции картинки
 
-// document.getElementById('next').onclick = function(){
-//     document.getElementById('123').classList.add(img[k + 1]);
-//     document.getElementById('last').classList.remove('last-btn');
-//     document.getElementById('last').classList.add('last-btn1');
-//     k = k + 1;   
-//     if(k == img.length){
-//         for(let i = 1; i< img.length; i++){
-//             document.getElementById('123').classList.remove(img[i]);
-//         }
-//         k = 0;
-//         document.getElementById('123').classList.add(img[k]);
-//     }
-// }
-
-// document.getElementById('last').onclick = function(){
-//     document.getElementById('123').classList.remove(img[k]);
-//     document.getElementById('123').classList.add(img[k - 1]);
-//     if(k < 2){
-//         k = img.length;
-//         document.getElementById('123').classList.add(img[k]);
-//     }
-//     else{
-//         k = k - 1;
-//     }
-// }
-
-let k = 0;
-
+// функция для оброботки нажатия на кнопку вперед
 document.getElementById('next').onclick = function(){
     document.getElementById('123').classList.add(img[k + 1]);
     k = k+1;
@@ -42,6 +15,7 @@ document.getElementById('next').onclick = function(){
     }
 }
 
+//функция для обработки нажатия на кнопку назад
 document.getElementById('last').onclick = function(){
     if (k == 0){
         k = 2;
